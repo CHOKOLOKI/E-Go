d89995271fed6567d9fe1b81bc65fc58d6e4fd85
Sergio Daguil <daguilgio@gmail.com>
2024-05-07 19:11:56 +0800

    add and setup redux

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { Provider } from 'react-redux';
+import { store } from './store';
 
 export default function App() {
   return (
     <Provider store={store}>
       <View style={styles.container}>
-        <Text>HEHE IM THE OWNER OF THIS APP AHDFKJABFAKSB -GIO</Text>
+        <Text>This is E-Go. Anita Max Wynn</Text>
         <StatusBar style="auto" />
       </View>
     </Provider>
