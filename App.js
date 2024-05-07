5b979b5408b309f6b55aa7cf261e5d92792ef669
Sergio Daguil <daguilgio@gmail.com>
2024-05-07 18:32:25 +0800

    First commit for E-Go with error on redux

--- App.js
+++ App.js
@@ -1,18 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Provider } from 'react-redux';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+      <View style={styles.container}>
+        <Text>HEHE IM THE OWNER OF THIS APP AHDFKJABFAKSB -GIO</Text>
+        <StatusBar style="auto" />
+      </View>
+    </Provider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flex: 1, 
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
